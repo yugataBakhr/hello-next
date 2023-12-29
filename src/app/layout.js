fa9e42416521f272648be25332592c30de0801f6
TooGood <ystg_found_them_snoopy@outlook.jp>
2023-12-29 12:44:36 +0900

    third commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./styles.css";
 
+export const dynamic = "force-dynamic";
+
 function RootLayout({ children }) {
   const timestamp = new Date().toLocaleString();
   return (
